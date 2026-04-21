2269faf7a48b2d32acf64c2154ea9d6cfbee2ce0
Eulalie2003 <amigoeulalie@gmail.com>
2026-04-21 17:52:06 +0000

    mise en forme et affichage des résultats

--- scripts/fonctions.py
+++ scripts/fonctions.py
@@ -2,8 +2,10 @@ from cartiflette import carti_download
 import pandas as pd
 import matplotlib.pyplot as plt
 import unicodedata
+from great_tables import GT, html
 
 
+# Nettoyage des données
 def nettoyer_texte(serie):
     """
     Automatise la standardisation d'une série de chaîne de caractères.
@@ -92,6 +94,38 @@ def ajouter_colonne_statut_precis(df):
     df_copy.loc[df_copy["freq_net"].notna(), "Statut"] = "Ouvert"
 
     return df_copy
+
+
+# Affichage des résultats
+def beau_tableau(df, titre):
+    """
+    Transforme une base de données en beau tableau.
+    """
+    tableau = (
+        GT(df)
+        .tab_header(title=html(f"<b>{titre}</b>"))
+    )
+
+    cols_numeriques = df.select_dtypes(include=['number']).columns.tolist()
+
+    if cols_numeriques:
+        tableau = tableau.fmt_number(
+            columns=cols_numeriques,
+            decimals=0,
+            sep_mark=" "
+        )
+
+    tableau = (
+        tableau.tab_options(
+            table_font_size="14px",
+            column_labels_font_weight="bold",
+            table_border_top_style="solid",
+            table_border_top_width="3px",
+            table_border_top_color="#444444"
+        )
+        .cols_align(align="center")
+    )
+    return tableau
 
 
 def tracer_comparaison_frequentation(bases_de_donnees, an='annee', freq='freq_net'):
